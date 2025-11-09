a89abb90b1bbb29db3471944bd170efb0c85b452
binyam-ta <binyamseleshe12@gmail.com>
2025-11-09 15:12:41 +0300

    redisdata

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
-        'PORT': os.environ.get('POSTGRES_PORT', 5433),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 }
 
