8f6137d9fa156eead6320b809c5fca4225be768b
binyam-ta <binyamseleshe12@gmail.com>
2025-11-09 15:25:06 +0300

    invalidation

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+    
+    def ready(self):
+        # Import signals to activate them
+        import properties.signals
+    
+    
